c26dcb94db5e57401c0be058c0790b87a9f29aa9
btlomax <103571051+btlomax@users.noreply.github.com>
2025-12-27 13:25:51 +0000

    Working on mirror, need to add mirror object script for rotation

--- Assets/Objects/PuzzleObjects/Optical/Mirror.cs
+++ Assets/Objects/PuzzleObjects/Optical/Mirror.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class Mirror : MonoBehaviour, IOpticalElement
 {
+    [SerializeReference]
+    private GameObject _mirror;
+
+    [Header("Mirror Settings")]
+    public float mirrorZAngle = 0f;
+
+    private void Awake()
+    {
+        mirrorZAngle = transform.rotation.eulerAngles.z;
+    }
+
     public bool Interact(Ray incomingRay, RaycastHit hit, BeamContext beam, out Ray outgoingRay)
     {
-        throw new System.NotImplementedException();
+        Vector3 normal = _mirror.transform.up;
+        Vector3 reflectedDir = Vector3.Reflect(incomingRay.direction, normal);
+
+        outgoingRay = new Ray(hit.point, reflectedDir);
+        Debug.DrawRay(outgoingRay.origin, outgoingRay.direction * 10f, beam.color, 1f);
+
+        return true;
     }
 }

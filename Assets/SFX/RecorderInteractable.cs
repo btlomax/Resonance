295be408e528a1e2a2e80406c95d02b58a31bd7d
btlomax <103571051+btlomax@users.noreply.github.com>
2026-01-11 22:53:52 +0000

    Aaaah wrong branch again...
    But got recording/not recording message in UI working

--- Assets/SFX/RecorderInteractable.cs
+++ Assets/SFX/RecorderInteractable.cs
@@ -1,8 +1,16 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RecorderInteractable : Interactable
 {
     private Recorder _recorder;
+
+    public TMP_Text recordingText;
+
+    [Header("For future use")]
+    public Image recordingLightOn;
+    public Image recordingLightOff;
     
     private void Awake()
     {
@@ -12,5 +20,27 @@ public class RecorderInteractable : Interactable
     public override void Interact(GameObject interactor)
     {
         _recorder.ToggleSimpleRecordingState();
+
+        if(_recorder.isRecording)
+        {
+            recordingText.SetText("Recording!");
+            return;
+        }
+
+        recordingText.SetText("Not recording...");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            recordingText.enabled = true;
+            recordingText.SetText("Not recording...");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        recordingText.enabled = false;
     }
 }
